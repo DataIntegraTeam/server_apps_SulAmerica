c6ac3219705ecbc9d8b2702b9bddbf0e1d3ebea8
DATA INTEGRA <marcelo.a.marcal@hotmail.com>
2022-04-05 21:35:30 -0300

    Cancelar Agendamento 1.3

--- src/modules/appointments/useCases/createAppointments/CreateAppointmentsUseCase.ts
+++ src/modules/appointments/useCases/createAppointments/CreateAppointmentsUseCase.ts
@@ -1,11 +1,18 @@
 import { Appointment } from '../../model/Appointment';
 import { IAppointmentsRepository } from '../../repositories/IAppointmentsRepository';
+import crypto from 'crypto';
 
 class CreateAppointmentsUseCase {
   constructor(private appointmentsRepository: IAppointmentsRepository) {}
 
-  async execute(data: Appointment): Promise<string> {
-    data.appointmentId = data.slotId;
+  async execute(data: Appointment): Promise<string | Error> {
+    const date = new Date(data.patient.birthDate);
+    const result = new Date().getFullYear() - date.getFullYear();
+    console.log(result);
+    if (!(result >= 18 && result <= 50)) {
+      throw new Error('preRequisiteAppointment');
+    }
+    data.appointmentId = String(crypto.randomInt(10000, 9000000));
     await this.appointmentsRepository.create(data);
 
     return data.appointmentId;
